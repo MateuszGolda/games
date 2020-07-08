f855922fca427da6b0d9a1d8a619dac005561929
Mateusz Golda <mateusz.golda1993@gmail.com>
2020-07-08 14:04:52 +0200

    implement hangman game

--- js/hangman.js
+++ js/hangman.js
@@ -1,1 +1,105 @@
+const programming_languages = [
+    "c",
+    "cplusplus",
+    "chash",
+    "erlang",
+    "go",
+    "haskell",
+    "java",
+    "javascript",
+    "kotlin",
+    "perl",
+    "php",
+    "python",
+    "r",
+    "ruby",
+    "rust",
+    "scala",
+    "sql",
+    "swift",
+    "typescript",
+]
 
+let answer = '';
+const maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+
+function randomWord() {
+    answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
+}
+
+function generateButtons() {
+    document.getElementById('keyboard').innerHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+        `
+      <button
+        class="btn btn-lg btn-primary m-2"
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
+}
+
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', 'true');
+
+    if (answer.indexOf(chosenLetter) >= 0) {
+        guessedWord();
+        checkIfGameWon();
+    } else if (answer.indexOf(chosenLetter) === -1) {
+        mistakes++;
+        updateMistakes();
+        checkIfGameLost();
+        updateHangmanPicture();
+    }
+}
+
+function updateHangmanPicture() {
+    document.getElementById('el' + (mistakes + 3)).style.backgroundColor = "black";
+}
+
+function checkIfGameWon() {
+    if (wordStatus.replace(/\s+/g, '') === answer) {
+        document.getElementById('keyboard').innerHTML = 'You Won!!!';
+    }
+}
+
+function checkIfGameLost() {
+    if (mistakes === maxWrong) {
+        document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
+        document.getElementById('keyboard').innerHTML = 'You Lost!!!';
+    }
+}
+
+function guessedWord() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join(' ');
+
+    document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+function updateMistakes() {
+    document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+function reset() {
+    mistakes = 0;
+    guessed = [];
+    for (let i = 4; i < 10; i++) {
+        document.getElementById('el' + (mistakes + i)).style.backgroundColor = "white";
+    }
+
+    randomWord();
+    guessedWord();
+    updateMistakes();
+    generateButtons();
+}
+
+document.getElementById('maxWrong').innerHTML = maxWrong + '';
+
+randomWord();
+generateButtons();
+guessedWord();
